faa6346efad354c333db313dc1b088cbebf7b885
eren.ates <eren.ates@ERENATES>
2024-02-19 09:44:25 +0300

    Add project files.

---
+++ RabbitMQExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Press enter");
+        Console.ReadLine();
+        var factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        factory.Port = 5672;
+        factory.UserName = "guest";
+        factory.Password = "guest";
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+            );
+
+        string message = "Message1111";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: "hello",
+            basicProperties: null,
+            body: body
+            );
+
+        Console.WriteLine($" [*] Send {message}");
+
+        Console.ReadLine();
+
+
+
+
+
+
+
+    }
+}
---
+++ RabbitMQExample2/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            queue: string.Empty,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+            );
+
+        Console.WriteLine("[*] Waiting for message..");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x]  {message}");
+        };
+
+        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+        Console.ReadLine();
+    }
+
+}
